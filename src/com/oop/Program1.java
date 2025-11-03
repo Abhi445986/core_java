644905a2553bc4c75edc92cd398a8c3900c658bb
Abhi445986 <abhishekyuva0012@gmail.com>
2025-11-03 19:20:11 +0530

    3-11-25

---
+++ src/com/oop/Program1.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+public class Program1 {
+    int a = 10;// IV no
+    static int c = 30;// sv no usages
+    public static void main(String[] args) {
+
+        int a = 10;
+
+    }
+
+}
